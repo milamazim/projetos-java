bcc1a45bbfbb7837bf2e949d5ab2d35d320b3f94
Camila Marques <milamazim@gmail.com>
2025-09-11 20:18:14 -0300

    Exercicio crescente

---
+++ crescente/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int x, y;
+
+    System.out.println("Digite dois números: ");
+    x = sc.nextInt();
+    y = sc.nextInt();
+
+    while(x != y) {
+      if (x > y) {
+        System.out.println("DECRESCENTE!");
+      }
+      else {
+        System.out.println("CRESCENTE!");
+      }
+      
+      System.out.println("Digite outros dois números:");
+      x = sc.nextInt();
+      y = sc.nextInt();
+
+    }
+
+    sc.close();
+  }
+}
